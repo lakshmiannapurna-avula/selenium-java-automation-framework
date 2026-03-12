c77bbc027df217ed98dc0747bad69b854b162316
lakshmiannapurna-avula <luckyavula9@gmail.com>
2026-03-12 17:34:21 -0400

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,1 +1,19 @@
+package tests;
 
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void verifyLoginpage() {
+        driver.get("https://web.com/login");
+
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.login("User", "Pass");
+
+        Assert.assertTrue(driver.getPageSource().contains("Your username is valid!"));
+    }
+}
